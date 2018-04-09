4c830069d1cfb18106519e071b4c6fff313027a5
xXHidekaneXx <38170815+xXHidekaneXx@users.noreply.github.com>
2018-04-08 19:33:02 -0600

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready',() => {
+  console.log('I am ready for sex!');
+});
+
+client.on('message', message => {
+if (message.content === '!fuck me'){
+    message.reply('yes daddy!');
+});
