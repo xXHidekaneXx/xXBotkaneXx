94a02a96023a926fcfbde2112c17d823a106f84f
Jasmine Blank <38170815+xXHidekaneXx@users.noreply.github.com>
2018-05-07 13:22:50 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,12 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready',() => {
-  console.log('I am ready for sex!');
-});
-
-client.on('message', message => {
-if (message.content === '!fuck me'){
-    message.reply('yes daddy!');
+  console.log('hi');
 });
   
 client.login(process.env.BOT_TOKEN);
