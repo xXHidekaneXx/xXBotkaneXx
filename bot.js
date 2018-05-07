9402ee27b51e6e13fcab1b591cd48389985600c3
Jasmine Blank <38170815+xXHidekaneXx@users.noreply.github.com>
2018-05-07 13:26:49 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,5 +4,18 @@ const client = new Discord.Client();
 client.on('ready',() => {
   console.log('hi');
 });
+
+function coinToss() {
+    return (Math.floor(Math.random() * 2) === 0);
+}
+client.on('message', (message) => {
+if(message.content.toLowerCase() == 'good morning' || message.content.toLowerCase() == 'i\'m so tired') {
+if (coinToss()) {
+message.reply('Good morning to you too!');
+} else {
+ message.reply('Good morning, how are you?');
+		}
+	}
+});
   
 client.login(process.env.BOT_TOKEN);
